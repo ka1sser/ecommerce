7d5954404a75eff25aad6664d3b0be1e3625b49c
ka1sser <serenokaisser.dev@gmail.com>
2025-07-10 00:15:52 +0800

    Adding some navigation elements and styling

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,9 +1,28 @@
+import { FiHeart } from 'react-icons/fi';
+import { AiOutlineShop, AiOutlineShoppingCart, AiOutlineUserAdd } from 'react-icons/ai';
 import './Nav.css'
 
 const Nav = () => {
-    return <div>
-        <h1>NAV</h1>
-    </div>
+    return <nav>
+        <div className='nav-container'>
+            <input
+                type='text'
+                className='search-input'
+                placeholder='Enter your shoes'
+            />
+        </div>
+        <div className='profile-container'>
+            <a href="#">
+                <FiHeart className='nav-icons'/>
+            </a>
+            <a href=''>
+                <AiOutlineShoppingCart className='nav-icons'/>
+            </a>
+            <a href=''>
+                <AiOutlineUserAdd className='nav-icons'/>
+            </a>
+        </div>
+    </nav>
 };
 
 export default Nav;
