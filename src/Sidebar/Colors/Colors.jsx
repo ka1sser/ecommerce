d6b4b882d812e40aedd0369442e9332fee59a65c
ka1sser <serenokaisser.dev@gmail.com>
2025-07-11 23:59:29 +0800

    Colors elements and styling

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,9 +1,35 @@
 
 
 const Colors = () => {
-    return <div>
-        <h1>Colors</h1>
-    </div>
+    return (
+      <div>
+        <h2 className="sidebar-title price-title">Colors</h2>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Black
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>White
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Red
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Green
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Blue
+        </label>
+      </div>
+    );
 }
 
 export default Colors;
