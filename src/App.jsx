0162ad9327f6f651800af28adc6529dc00292004
ka1sser <serenokaisser.dev@gmail.com>
2025-07-11 22:40:25 +0800

    Sidebar elements and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import Nav from './Navigation/Nav'
 import Products from './Products/Products'
 import Recommended from './Recommended/Recommended'
+import Sidebar from './Sidebar/Sidebar'
 
 function App() {
 
   return <div>
+    <Sidebar/>
     <Nav />
     <Recommended/>
     <Products />
