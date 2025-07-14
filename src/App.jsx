90b09570909eec5eaf70873dff35abc4459ed8b8
ka1sser <serenokaisser.dev@gmail.com>
2025-07-14 15:22:56 +0800

    Added and used props to pass in data

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,76 @@ import Nav from './Navigation/Nav'
 import Products from './Products/Products'
 import Recommended from './Recommended/Recommended'
 import Sidebar from './Sidebar/Sidebar'
+import Card from './components/Card'
+
+// ------ Database ------
+import products from './db/data'
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-  return <div>
-    <Sidebar/>
-    <Nav />
-    <Recommended/>
-    <Products />
-  </div>
+  // ------ Input Filter ------
+  const [query, setQuery] = useState("");
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const filtereditems = products.filter((products) =>
+    products.title.toLocaleLowerCase().indexOf(query.toLocaleUpperCase() !== -1)
+  );
+
+  // ------ Radio Filters ------
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  //  ------ Button Filter
+  const handleClick = e => {
+    setSelectedCategory(e.target.value)
+  }
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products
+
+    // Filtering input items
+    if (query) {
+      filteredData = filtereditems
+    }
+
+    // Selected Filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(({ category, color, company, newPrice, title }) =>
+        category === selected ||
+        color === selected ||
+        company === selected ||
+        newPrice === selected ||
+        title === selected
+      );
+    }
+
+    return filteredProducts.map(({ img, title, star, reviews,prevPrice, newPrice }) => (
+      <Card
+        key={Math.random()}
+        img={img}
+        title={title}
+        star={star}
+        reviews={reviews}
+        prevPrice={prevPrice}
+        newPrice={newPrice}
+      />
+    ));
+  }
+
+  filteredData(products, selectedCategory, query)
+
+  return (
+    <div>
+      <Sidebar handleChange={handleChange}/>
+      <Nav />
+      <Recommended />
+      <Products />
+    </div>
+  );
 }
 
 export default App
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,17 @@
 
-const Input = () => {
-    return (
-      <label className="sidebar-label-container">
-        <input type="radio" name="test2" />
-        <span className="checkmark"></span>All
-      </label>
-    );
-}
+const Input = ({ handleChange,value,title, name, color}) => {
+  return (
+    <label className="sidebar-label-container">
+      <input
+        onChange={handleChange}
+        type="radio"
+        value={value}
+        name={name}
+      />
+      <span className="checkmark" style={{ backgroundColor: color }}></span>
+      {title}
+    </label>
+  );
+};
 
 export default Input;
