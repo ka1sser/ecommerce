70959c38f88f9b39cecab93b133f0791e6cbb3b0
ka1sser <serenokaisser.dev@gmail.com>
2025-07-15 23:16:33 +0800

    Finished with project

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,82 @@
-import { useState } from 'react'
-import Nav from './Navigation/Nav'
-import Products from './Products/Products'
-import Recommended from './Recommended/Recommended'
-import Sidebar from './Sidebar/Sidebar'
-import Card from './components/Card'
+import { useState } from "react";
 
-// ------ Database ------
-import products from './db/data'
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import products from "./db/data";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+import Card from "./components/Card";
+import "./index.css";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
 
-  // ------ Input Filter ------
+  // ----------- Input Filter -----------
   const [query, setQuery] = useState("");
-  const handleInputChange = (e) => {
-    setQuery(e.target.value);
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
   };
 
-  const filtereditems = products.filter((products) =>
-    products.title.toLocaleLowerCase().indexOf(query.toLocaleUpperCase() !== -1)
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
   );
 
-  // ------ Radio Filters ------
-  const handleChange = (e) => {
-    setSelectedCategory(e.target.value);
+  // ----------- Radio Filtering -----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
   };
 
-  //  ------ Button Filter
-  const handleClick = e => {
-    setSelectedCategory(e.target.value)
-  }
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
   function filteredData(products, selected, query) {
-    let filteredProducts = products
+    let filteredProducts = products;
 
-    // Filtering input items
+    // Filtering Input Items
     if (query) {
-      filteredData = filtereditems
+      filteredProducts = filteredItems;
     }
 
-    // Selected Filter
+    // Applying selected filter
     if (selected) {
-      filteredProducts = filteredProducts.filter(({ category, color, company, newPrice, title }) =>
-        category === selected ||
-        color === selected ||
-        company === selected ||
-        newPrice === selected ||
-        title === selected
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
       );
     }
 
-    return filteredProducts.map(({ img, title, star, reviews,prevPrice, newPrice }) => (
-      <Card
-        key={Math.random()}
-        img={img}
-        title={title}
-        star={star}
-        reviews={reviews}
-        prevPrice={prevPrice}
-        newPrice={newPrice}
-      />
-    ));
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
   }
 
-  const result = filteredData(products, selectedCategory, query)
+  const result = filteredData(products, selectedCategory, query);
 
   return (
-    <div>
+    <>
       <Sidebar handleChange={handleChange} />
-      <Nav query={query} handleInputChange={handleInputChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
       <Recommended handleClick={handleClick} />
       <Products result={result} />
-    </div>
+    </>
   );
 }
 
-export default App
+export default App;
